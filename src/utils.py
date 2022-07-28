fixed pwm sign bug

src/utils.py
@@ -209,10 +209,13 @@ class KamigamiInterface:
         self.next_states = []
 
     def get_new_pwm(self, pwm, before, after):
-        if pwm < before:
-            new_pwm = pwm * after / before
+        positive = (pwm > 0)
+        abs_pwm = abs(pwm)
+        if abs_pwm < before:
+            new_abs_pwm = abs_pwm * after / before
         else:
-            new_pwm = (pwm - before) / before * (1 - after) + after
+            new_abs_pwm = (abs_pwm - before) / before * (1 - after) + after
+        new_pwm = new_abs_pwm * (positive * 2 - 1)
         return np.clip(new_pwm, *self.action_range[:, 0])
 
     def remap_cmd(self, cmd, robot):