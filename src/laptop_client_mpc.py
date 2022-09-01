scaling fix

src/laptop_client_mpc.py
@@ -18,12 +18,15 @@ from tf.transformations import euler_from_quaternion
 
 SAVE_PATH = "/home/bvanbuskirk/Desktop/MPCDynamicsKamigami/sim/data/real_data_online400.npz"
 
+np.random.seed(0)
+import torch; torch.manual_seed(0)
+
+
 class RealMPC():
     def __init__(self, robot_id, object_id, mpc_steps, mpc_samples, n_rollouts, tolerance, lap_time, calibrate, plot,
-                 new_buffer, pretrain, robot_goals, scale, mpc_softmax):
+                 new_buffer, pretrain, robot_goals, scale, mpc_softmax, save_freq, online, mpc_refine_iters, pretrain_samples):
         self.halted = False
         self.initialized = False
-        self.epsilon = 0.0
         self.steps = 0
         self.gradient_steps = 2
         self.random_steps = 0 if pretrain else 500
@@ -32,6 +35,10 @@ class RealMPC():
         self.robot_id = robot_id
         self.object_id = object_id
         self.mpc_softmax = mpc_softmax
+        self.save_freq = save_freq
+        self.online = online
+        self.mpc_refine_iters = mpc_refine_iters
+        self.pretrain_samples = pretrain_samples
         self.use_object = (self.object_id >= 0)
 
         max_pwm = 0.999
@@ -51,7 +58,6 @@ class RealMPC():
         self.n_wait_updates = 1
         self.n_clip = 3
         self.flat_lim = 0.6
-        self.save_freq = 50
 
         if os.path.exists("/home/bvanbuskirk/Desktop/MPCDynamicsKamigami/replay_buffers/buffer.pkl") and not new_buffer:
             with open("/home/bvanbuskirk/Desktop/MPCDynamicsKamigami/replay_buffers/buffer.pkl", "rb") as f:
@@ -62,7 +68,7 @@ class RealMPC():
 
         rospy.init_node("laptop_client_mpc")
 
-        robot_id = self.robot_id if False else 0
+        robot_id = self.robot_id if False else 1
         print(f"waiting for robot {robot_id} service")
         rospy.wait_for_service(f"/kami{robot_id}/server")
         self.service_proxy = rospy.ServiceProxy(f"/kami{robot_id}/server", CommandAction)
@@ -103,12 +109,12 @@ class RealMPC():
         # self.heading_diff_weight = 0.0
 
         self.perp_weight = 0.
-        self.heading_weight = 0.
+        self.heading_weight = 0.0
         self.dist_weight = 1.
         self.norm_weight = 0.0
         self.dist_bonus_weight = 0.
         self.sep_weight = 0.
-        self.discrim_weight = 0.
+        self.discrim_weight = 0.0
         self.heading_diff_weight = 0.0
 
         self.plot_robot_states = []
@@ -130,8 +136,8 @@ class RealMPC():
         if pretrain:
             self.train_model()
 
-        for g in self.agent.models[0].optimizer.param_groups:
-            g['lr'] = 3e-4
+        # for g in self.agent.models[0].optimizer.param_groups:
+        #     g['lr'] = 3e-4
 
         np.set_printoptions(suppress=True)
         self.initialized = True
@@ -154,19 +160,27 @@ class RealMPC():
             object_left_state, object_right_state = left_state[3:], right_state[3:]
             true_object_vector = (object_left_state - object_right_state)[:2]
             true_object_angle = np.arctan2(true_object_vector[1], true_object_vector[0])
-            measured_object_angle = object_left_state[2]            
+            measured_object_angle = object_left_state[2]
             tag_offsets[self.object_id] = true_object_angle - measured_object_angle
 
         np.save(self.tag_offset_path, tag_offsets)
 
     def train_model(self):
         idx = self.replay_buffer.capacity if self.replay_buffer.full else self.replay_buffer.idx
-        states = self.replay_buffer.states[:idx-1]
-        actions = self.replay_buffer.actions[:idx-1]
-        next_states = self.replay_buffer.states[1:idx]
+
+        # states = self.replay_buffer.states[:idx-1]
+        # actions = self.replay_buffer.actions[:idx-1]
+        # next_states = self.replay_buffer.states[1:idx]
+
+        # random_idx = np.random.permutation(len(states))[:self.pretrain_samples]
+        # states = states[random_idx]
+        # actions = actions[random_idx]
+        # next_states = next_states[random_idx]
+
+        states, actions, next_states = self.replay_buffer.sample(self.pretrain_samples)
 
         training_losses, test_losses, discrim_training_losses, discrim_test_losses, test_idx = self.agent.train(
-                states, actions, next_states, set_scalers=True, epochs=400, discrim_epochs=5, batch_size=1000, use_all_data=False)
+                states, actions, next_states, set_scalers=True, epochs=800, discrim_epochs=5, batch_size=1000, use_all_data=False)
 
         training_losses = np.array(training_losses).squeeze()
         test_losses = np.array(test_losses).squeeze()
@@ -178,6 +192,24 @@ class RealMPC():
 
         print("\nMIN DISCRIM TEST LOSS EPOCH:", discrim_test_losses.argmin())
         print("MIN DISCRIM TEST LOSS:", discrim_test_losses.min())
+
+        state_delta = self.agent.dtu.state_delta_xysc(states, next_states)
+
+        test_state, test_action = states[test_idx], actions[test_idx]
+        test_state_delta = dtu.dcn(state_delta[test_idx])
+
+        pred_state_delta = dtu.dcn(self.agent.models[0](test_state, test_action, sample=False))
+        # pred_state_delta = agent.get_prediction(test_states, test_actions, sample=False, scale=args.scale, delta=True, use_ensemble=False)
+
+        error = abs(pred_state_delta - test_state_delta)
+        print("\nERROR MEAN:", error.mean(axis=0))
+        print("ERROR STD:", error.std(axis=0))
+        print("ERROR MAX:", error.max(axis=0))
+        print("ERROR MIN:", error.min(axis=0))
+
+        diffs = abs(test_state_delta)
+        print("\nACTUAL MEAN:", diffs.mean(axis=0))
+        print("ACTUAL STD:", diffs.std(axis=0))
 
         fig, axes = plt.subplots(1, 4)
         axes[0].plot(np.arange(len(training_losses)), training_losses, label="Training Loss")
@@ -287,7 +319,7 @@ class RealMPC():
 
                 if self.plot:
                     self.plot_states_and_goals()
-            elif self.dist_to_start() < self.tolerance:
+            elif self.dist_to_start() < self.tolerance and (self.pretrain or self.steps >= self.random_steps):
                 self.started = True
                 self.last_goal = self.get_goal()
                 self.time_elapsed = self.duration / 2
@@ -336,14 +368,17 @@ class RealMPC():
             return False
 
         self.collect_training_data(state, action)
-        self.update_model_online()
+
+        if self.online:
+            self.update_model_online()
+
         self.check_rollout_finished()
 
         self.steps += 1
         return True
 
     def get_state(self, wait=True):
-        if self.n_avg_states > 1 and not wait:
+        if self.n_avg_states > 1 and wait:
             robot_states, object_states = [], []
             while len(robot_states) < self.n_avg_states:
                 if wait:
@@ -377,16 +412,24 @@ class RealMPC():
         state_for_last_goal = state[:3] if self.robot_goals else state[3:]
         last_goal = self.last_goal if self.started else state_for_last_goal
 
-        if self.steps >= self.random_steps and np.random.rand() > self.epsilon:
+        if self.steps >= self.random_steps:
             action = self.agent.mpc_action(state, last_goal, goal, self.action_range, n_steps=self.mpc_steps,
                                            n_samples=self.mpc_samples, perp_weight=self.perp_weight,
                                            heading_weight=self.heading_weight, dist_weight=self.dist_weight,
                                            norm_weight=self.norm_weight, sep_weight=self.sep_weight if self.started else 0.,
                                            discrim_weight=self.discrim_weight, heading_diff_weight=self.heading_diff_weight,
-                                           dist_bonus_weight=self.dist_bonus_weight, robot_goals=self.robot_goals, mpc_softmax=self.mpc_softmax).detach().numpy()
+                                           dist_bonus_weight=self.dist_bonus_weight, robot_goals=self.robot_goals,
+                                           mpc_softmax=self.mpc_softmax, mpc_refine_iters=self.mpc_refine_iters).detach().numpy()
+            self.time_elapsed += self.duration if self.started else 0
         else:
             print("TAKING RANDOM ACTION")
-            action = np.random.uniform(*self.action_range, size=(1, self.action_range.shape[-1])).squeeze()
+            if self.steps % 11 == 0:
+                if self.steps % 2 == 0:
+                    action = np.array([0.99, -0.99])
+                else:
+                    action = np.array([-0.99, 0.99])
+            else:
+                action = np.random.uniform(*self.action_range, size=(1, self.action_range.shape[-1])).squeeze()
 
         action = np.clip(action, *self.action_range)
         action_req = RobotCmd()
@@ -395,7 +438,6 @@ class RealMPC():
         action_req.duration = self.duration
 
         self.service_proxy(action_req, f"kami{self.robot_id}")
-        self.time_elapsed += action_req.duration if self.started else 0
 
         time = rospy.get_time()
         bool_idx = (self.stamped_losses[:, 0] > time - action_req.duration) & (self.stamped_losses[:, 0] < time)
@@ -435,8 +477,8 @@ class RealMPC():
 
     def get_goal(self, random=True):
         t_rel = (self.time_elapsed % self.lap_time) / self.lap_time
-        if not self.started and random:
-            t_rel += np.random.uniform(0, 0.01)
+        # if not self.started and random:
+        #     t_rel += np.random.uniform(0, 0.01)
 
         if t_rel < 0.5:
             theta = t_rel * 2 * 2 * np.pi
@@ -506,7 +548,6 @@ class RealMPC():
                 pkl.dump(self.replay_buffer, f)
 
     def update_model_online(self):
-        return
         if (self.replay_buffer.full or self.replay_buffer.idx > 50) and (self.pretrain or (not self.pretrain and self.steps >= self.random_steps)):
             for model in self.agent.models:
                 for _ in range(self.gradient_steps):
@@ -530,9 +571,14 @@ if __name__ == '__main__':
     parser.add_argument('-robot_goals', action='store_true')
     parser.add_argument('-scale', action='store_true')
     parser.add_argument('-mpc_softmax', action='store_true')
+    parser.add_argument('-online', action='store_true')
+    parser.add_argument('-save_freq', type=int, default=50)
+    parser.add_argument('-mpc_refine_iters', type=int, default=1)
+    parser.add_argument('-pretrain_samples', type=int, default=500)
 
     args = parser.parse_args()
 
     r = RealMPC(args.robot_id, args.object_id, args.mpc_steps, args.mpc_samples, args.n_rollouts, args.tolerance,
-                args.lap_time, args.calibrate, args.plot, args.new_buffer, args.pretrain, args.robot_goals, args.scale, args.mpc_softmax)
+                args.lap_time, args.calibrate, args.plot, args.new_buffer, args.pretrain, args.robot_goals, args.scale,
+                args.mpc_softmax, args.save_freq, args.online, args.mpc_refine_iters, args.pretrain_samples)
     r.run()

src/real_mpc_dynamics.py
@@ -18,6 +18,30 @@ import data_utils as dtu
 pi = torch.pi
 device = torch.device("cpu")
 
+# rows: (dist, perp, heading, total)
+# cols: (mean, std, min, max)
+# DATA: [[0.04219189 0.03084236 0.00235231 0.16994911]
+#  [0.02027672 0.02345186 0.00000654 0.16348039]
+#  [0.27874293 0.21315557 0.0010314  1.14644289]
+#  [0.04219189 0.03084236 0.00235231 0.16994911]]
+
+# 200 samples
+# rows: (dist, perp, heading, total)
+# cols: (mean, std, min, max)
+# DATA: [[0.12418899 0.09081132 0.0149498  0.40099999]
+#  [0.06070356 0.07252411 0.00001037 0.26366729]
+#  [0.55921582 0.44112111 0.00212717 1.54231215]
+#  [0.12418899 0.09081132 0.0149498  0.40099999]]
+
+# 500 samples
+# rows: (dist, perp, heading, total)
+# cols: (mean, std, min, max)
+# DATA: [[0.24072483 0.14470543 0.01090682 0.55046976]
+#  [0.1385723  0.1326446  0.00016232 0.4843801 ]
+#  [0.7081059  0.46009894 0.00321484 1.56313467]
+#  [0.24072483 0.14470543 0.01090682 0.55046976]]
+
+
 
 class DynamicsNetwork(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim=256, hidden_depth=2, lr=1e-3, dropout=0.5, std=0.02, dist=True, use_object=False, scale=True):
@@ -32,7 +56,7 @@ class DynamicsNetwork(nn.Module):
         layers += [nn.Linear(hidden_dim, output_dim)]
         self.model = nn.Sequential(*layers)
         self.output_dim = output_dim
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, amsgrad=True, weight_decay=1e-4)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, amsgrad=True)
         self.scale = scale
         self.dist = dist
         self.use_object = use_object
@@ -49,7 +73,7 @@ class DynamicsNetwork(nn.Module):
             if hasattr(m.bias, 'data'):
                 m.bias.data.fill_(0.0)
 
-    def forward(self, state, action, scale=True, sample=False, return_dist=False):
+    def forward(self, state, action, scale_input=True, sample=False, return_dist=False):
         state, action = dtu.as_tensor(state, action)
 
         if len(state.shape) == 1:
@@ -58,7 +82,7 @@ class DynamicsNetwork(nn.Module):
             action = action[None, :]
 
         state_sc = self.dtu.state_to_model_input(state)
-        if self.scale and scale:
+        if self.scale and scale_input:
             state_sc, action = self.get_scaled_input(state_sc, action)
 
         state_action = torch.cat([state_sc, action], dim=1).float()
@@ -86,8 +110,9 @@ class DynamicsNetwork(nn.Module):
 
         if self.dist:
             dist = self(state, action, return_dist=True)
-            state_delta_scaled = self.get_scaled_output(state_delta).detach()
-            loss = -dist.log_prob(state_delta_scaled).sum()
+            if self.scale:
+                state_delta = self.get_scaled_output(state_delta).detach()
+            loss = -dist.log_prob(state_delta).sum()
         else:
             pred_state_delta = self(state, action)
             loss = F.mse_loss(pred_state_delta, state_delta, reduction='mean')
@@ -104,8 +129,8 @@ class DynamicsNetwork(nn.Module):
     def set_scalers(self, state, action, next_state):
         state, action, next_state = dtu.dcn(state, action, next_state)
         input_state = self.dtu.state_to_model_input(state)
-        state_delta = self.dtu.state_delta_xysc(state, next_state)
         state_action = np.concatenate([input_state, action], axis=1)
+        state_delta = self.dtu.state_delta_xysc(state, next_state)
 
         self.input_scaler = StandardScaler().fit(state_action)
         self.output_scaler = StandardScaler().fit(state_delta)
@@ -173,7 +198,7 @@ class MPCAgent:
         assert ensemble > 0
         input_dim = output_dim = 8 if use_object else 4
 
-        self.models = [DynamicsNetwork(input_dim, output_dim, hidden_dim=hidden_dim, hidden_depth=hidden_depth, lr=lr, dropout=dropout, std=std, dist=dist, use_object=use_object)
+        self.models = [DynamicsNetwork(input_dim, output_dim, hidden_dim=hidden_dim, hidden_depth=hidden_depth, lr=lr, dropout=dropout, std=std, dist=dist, use_object=use_object, scale=scale)
                                 for _ in range(ensemble)]
         for model in self.models:
             model.to(device)
@@ -189,11 +214,10 @@ class MPCAgent:
 
     def mpc_action(self, state, prev_goal, goal, action_range, n_steps=10, n_samples=1000, perp_weight=0.0,
                    heading_weight=0.0, dist_weight=1.0, norm_weight=0.0, sep_weight=0.0, discrim_weight=0.0, heading_diff_weight=0.0,
-                   dist_bonus_weight=0.0, robot_goals=False, mpc_softmax=False):
+                   dist_bonus_weight=0.0, robot_goals=False, mpc_softmax=False, mpc_refine_iters=5):
         state, prev_goal, goal, action_range = dtu.as_tensor(state, prev_goal, goal, action_range)
         original_state = state
 
-        mpc_refine_iters = 5
         gamma = 35000
         alpha = 0.9
         n_best_losses = 10
@@ -231,7 +255,7 @@ class MPCAgent:
                                     + discrim_weight * discrim_loss + 0 * (dist_bonus + 10 * dist_bonus2 + 100 * dist_bonus3)
 
             trajectory_losses = all_losses.sum(dim=0)
-            trajectory_losses = trajectory_losses / trajectory_losses.sum()
+            # trajectory_losses = trajectory_losses / trajectory_losses.sum()
 
             if mpc_refine_iters > 1:
                 _, best_losses_idx = torch.topk(-trajectory_losses, n_best_losses)
@@ -439,7 +463,7 @@ class MPCAgent:
                 tqdm.write(f"{i+1}: train loss: {train_loss_mean:.5f} | test loss: {test_loss_mean:.5f}")
 
             print("\n\nTRAINING DISCRIMINATOR\n")
-            batch_size = 100
+            # batch_size = 100
             n_batches = int(np.ceil(len(train_state) / batch_size))
 
             for i in tqdm(range(-1, discrim_epochs), desc="Epoch", position=0, leave=False):