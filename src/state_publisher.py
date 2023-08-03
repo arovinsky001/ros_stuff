1f5d448b6dbab5ccd6247ac6da3667a23ca6ce95
Aaron Rovinsky <arovinsky001@gmail.com>
2023-08-03 08:04:36 -0700

    state publisher improved base not found warning and velocity

--- src/state_publisher.py
+++ src/state_publisher.py
@@ -26,6 +26,8 @@ class StatePublisher:
         self.id_to_state = {id: np.zeros(3) for id in ids}
         self.id_to_past_states_stamped = {id: np.zeros((3, 4)) for id in ids}
 
+        self.not_found = False
+
         rospy.init_node("state_publisher")
 
         print("setting up tf buffer/listener")
@@ -43,8 +45,13 @@ class StatePublisher:
     def update(self, msg):
         found_ids = [marker.id for marker in msg.markers]
         if not self.base_id in found_ids:
+            self.not_found = True
             print("\nBASE MARKER NOT FOUND\n")
             return
+
+        if self.not_found:
+            self.not_found = False
+            print("\nBASE MARKER FOUND\n")
 
         for marker in msg.markers:
             if marker.id in self.id_to_state and marker.id != self.base_id:
@@ -102,20 +109,23 @@ class StatePublisher:
 
     def compute_vel_from_past_states(self):
         velocities = {}
-        eps = 0.00001
+        eps = 1e-8
         for id in [self.object_id, *self.robot_ids]:
             past_states = self.id_to_past_states_stamped[id]
             v1 = past_states[0] - past_states[1]
             v2 = past_states[1] - past_states[2]
+            v3 = past_states[0] - past_states[2]
 
             # get signed difference for yaw
             v1[2] = (v1[2] + np.pi) % (2 * np.pi) - np.pi
             v2[2] = (v2[2] + np.pi) % (2 * np.pi) - np.pi
+            v3[2] = (v3[2] + np.pi) % (2 * np.pi) - np.pi
 
             # divide by time delta to get velocity
             v1 = v1[:-1] / (v1[-1] + eps)
             v2 = v2[:-1] / (v2[-1] + eps)
-            velocities[id] = ((v1 + v2) / 2)
+            v3 = v3[:-1] / (v3[-1] + eps)
+            velocities[id] = ((v1 + v2 + v3) / 3)
 
         return velocities
 
